3a39ef7a1c1ed2e4781ad63f859a2ce08d6c038b
Jác Lindley <70286995+jaclindley@users.noreply.github.com>
2022-08-01 15:04:08 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://357d16f9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
